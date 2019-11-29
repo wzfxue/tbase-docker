c1c439162ca8169d4aa1382d6f48ef0fae167b9a
wangzhoufeng <1039580989@qq.com>
2019-11-29 17:58:14 +0800

    添加创建组和sharding表

--- src/shell/configs/initpgxc.sh
+++ src/shell/configs/initpgxc.sh
@@ -15,3 +15,9 @@ echo "初始化完毕,测试是否成功"
 psql -p 30004 -h 172.42.0.11 -U postgres<<EOF
 select * from pgxc_node;
 EOF
+
+echo "创建default group以及sharding表"
+psql -p 30004 -h 172.42.0.11 -U postgres<<EOF
+create default node group default_group with (dn001,dn002);
+create sharding group to group default_group;
+EOF
